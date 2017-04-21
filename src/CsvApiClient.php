4e6824a693921ea5cb927d767536015fb0f43922
James Harper <jameswilliamharper@gmail.com>
2017-04-21 15:44:05 +0100

    Implement CsvApiClient

---
+++ src/CsvApiClient.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * This file is part of the PayBreak\ApiClient package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace PayBreak\ApiClient;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class CsvApiClient
+ * @author JH
+ * @package PayBreak\ApiClient
+ */
+class CsvApiClient extends ApiClient
+{
+    /**
+     * @author JH
+     * @param ResponseInterface $response
+     * @return array
+     * @throws WrongResponseException
+     */
+    protected function processResponse(ResponseInterface $response)
+    {
+        if ($response->getStatusCode() == 204) {
+            return [];
+        }
+
+        if (strpos($response->getHeaderLine('Content-Type'), 'csv') !== false) {
+            return ['csv' => $response->getBody()->getContents()] ;
+        }
+
+        throw new WrongResponseException('Response body was malformed csv', $response->getStatusCode());
+    }
+}
